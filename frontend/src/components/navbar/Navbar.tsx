1e302f4bc00bbc6830fd83aa869c78b1b3992d73
Yulia Shyshka <shyshka20@gmail.com>
2023-06-01 21:21:52 +0300

    add burger menu

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,27 +1,35 @@
+import { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
 
   return (
     <nav className="navbar">
-      <div className="navbar__logo">
-        <h2>Choose products in shops</h2>
+      <div className="box">
+        <div className="logo_image">
+          <h2>Choose products in shops</h2>
+        </div>
+        <ul className={nav ? 'menu active' : 'menu'}>
+          <li onClick={() => setNav(false)}>
+            <Link to="/" className="cart__link" >
+              Shop
+            </Link>
+          </li>
+          <li onClick={() => setNav(false)}>
+            <Link to="/cart" className="cart__link">
+              <i className="fas fa-shopping-cart"></i>
+              <span>Shopping cart</span>
+            </Link>
+          </li>
+        </ul>
+        <div onClick={() => setNav(!nav)} className={"mobile_btn"}>
+          {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+        </div>
       </div>
-
-      <ul className="navbar__links">
-        <li>
-          <Link to="/" className="cart__link">Shop</Link>
-        </li>
-        <li>
-          <Link to="/cart" className="cart__link">
-            <i className="fas fa-shopping-cart"></i>
-            <span>
-              Shopping cart
-            </span>
-          </Link>
-        </li>
-      </ul>
     </nav>
   );
 };
